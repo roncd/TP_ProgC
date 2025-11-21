5a7c20cdce850549c093b89d0641d50da5bd4771
Rosalie <rosalie.nicaud@gmail.com>
2025-11-21 10:25:02 +0100

    edit cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main() {
-double pi = 3.1416;
+float rayon = 3.5;
+double pi = 3.14159;
 double r = 6;  
-    printf("L'air du cercle = %f", pi*(r*r));
-   printf("Le périmètre du cercle = %f", pi*(r+r));
+    printf("L'air du cercle = %f", pi*(rayon*rayon));
+   printf("Le périmètre du cercle = %f", 2*pi*rayon);
 }
 
